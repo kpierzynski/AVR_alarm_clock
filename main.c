b1867ba41548bd7f6743932c88a3becafb05044a
kpierzynski <kpierzynski@hotmail.com>
2022-10-08 21:04:29 +0200

    Display multiplexing

---
+++ main.c
@@ -0,0 +1,91 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define LEN 16
+uint8_t nums[LEN] = {	0b00100000,	//0
+			0b10111010,	//1
+			0b01001000,	//2
+			0b00011000,	//3
+			0b10010010,	//4
+			0b00010100,	//5
+			0b00000100,	//6
+			0b10111000,	//7
+			0b00000000,	//8
+			0b00010000,	//9
+			0b10000000,	//A
+			0b00000110,	//B
+			0b01100100,	//C
+			0b00001010,	//D
+			0b01000100,	//E
+			0b11000100,	//F
+};
+
+#define DISPLAY_LEN 4
+volatile uint8_t display[DISPLAY_LEN] = {0,0,0,0};
+volatile uint8_t tick = 0;
+
+typedef struct {
+	uint8_t hour;
+	uint8_t min;
+} time_t;
+
+time_t time;
+
+void set_time( time_t time ) {
+	display[0] = time.hour/10;
+	display[1] = time.hour%10;
+
+	display[2] = time.min/10;
+	display[3] = time.min%10;
+}
+
+ISR( TIMER0_COMPA_vect ) {
+	static uint8_t digit = 0;
+
+	PORTC = (1<<digit);
+	PORTB = ( nums[ display[digit] ] | tick);
+
+	PORTD = (PORTD & 0b00011111) | ((PIND & 0b00011100)<<3);
+
+	digit = (digit+1) % DISPLAY_LEN;
+}
+
+int main() {
+
+	//TIMER FOR MULTIPLEXING
+	TCCR0A |= (1<<WGM01);
+	TCCR0B |= (1<<CS02)|(1<<CS00);
+	OCR0A = 32;
+	TIMSK0 |= (1<<OCIE0A);
+
+	//DOTS
+	DDRD |= (1<<PD5)|(1<<PD6)|(1<<PD7);
+
+	//BUTTONS
+	DDRD &= ~(1<<PD2);
+	DDRD &= ~(1<<PD3);
+	DDRD &= ~(1<<PD4);
+	PORTD |= (1<<PD2)|(1<<PD3)|(1<<PD4);
+
+	//NUMBER DATA PORT DIRECTION (use whole port. PB0 for dots)
+	DDRB = 0xFF;
+
+	//PLEXER TRANSISTOR DIRECTION
+	DDRC |= (1<<PC0)|(1<<PC1)|(1<<PC2)|(1<<PC3);
+
+	time.hour = 12;
+	time.min = 39;
+
+	sei();
+	while( 1 ) {
+		set_time( time );
+		time.min++;
+		tick = (tick+1)%2;
+
+
+		_delay_ms(1000);
+	}
+
+	return 0;
+}
