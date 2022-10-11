95fb82dbb42617c52ffa27ad7e254130e21f8d66
kpierzynski <kpierzynski@hotmail.com>
2022-10-11 21:50:52 +0200

    Alarm setter prototype

--- main.c
+++ main.c
@@ -29,24 +29,26 @@ uint8_t nums[LEN] = {	0b00100000,	//0
 };
 
 #define DISPLAY_LEN	4
-volatile uint8_t display[DISPLAY_LEN];
+volatile uint8_t main_screen[DISPLAY_LEN];
+volatile uint8_t alarm_screen[DISPLAY_LEN];
+
+volatile uint8_t * display = main_screen;
 
 #define ALARM_LEN	3
 alarm_t alarms[ALARM_LEN];
 time_t time;
 
-volatile uint8_t mode;
+uint8_t mode;
 
 volatile uint8_t tick = 0;
 void clock_tick() {
 	tick ^= 1;
 
 	clock_time( &time );
-	display[0] = time.hour/10;
-	display[1] = time.hour%10;
-	display[2] = time.min/10;
-	display[3] = time.min%10;
-
+	main_screen[0] = time.hour/10;
+	main_screen[1] = time.hour%10;
+	main_screen[2] = time.min/10;
+	main_screen[3] = time.min%10;
 }
 
 volatile uint8_t blink_mask;
@@ -64,15 +66,81 @@ ISR( TIMER0_COMPA_vect ) {
 }
 
 void next_mode() {
+	Timer3 = 1000;
+
+	uint8_t current_alarm_index = mode / 3;
+	uint8_t type = mode % 3;
+
+	alarm_t * alarm = &alarms[current_alarm_index];
+
+	if( type == 0 ) {
+		blink_mask = 0b0000;
+		alarm_screen[0] = 0xA;
+		alarm_screen[1] = current_alarm_index;
+		alarm_screen[2] = 0x11;
+		alarm_screen[3] = 0x11;
+	} else {
+		if( type == 1 )
+			blink_mask = 0b0011;
+		else blink_mask = 0b1100;
+
+		alarm_screen[0] = alarm->time.hour/10;
+		alarm_screen[1] = alarm->time.hour%10;
+		alarm_screen[2] = alarm->time.min/10;
+		alarm_screen[3] = alarm->time.min%10;
+	}
+	display = alarm_screen;
+	mode = (mode+1)%(ALARM_LEN*3);
 }
 
-void up_tick() {
+void change(uint8_t d) {
+	Timer3 = 1000;
+
+	if( display == main_screen ) return;
+
+	uint8_t type = (mode-1) % 3;
+	uint8_t current_alarm_index = (mode-1) / 3;
+
+	alarm_t * alarm = &alarms[current_alarm_index];
+
+	switch( type ) {
+		case 0:
+		{
+			uint8_t armed = (alarm->armed ^= 1);
+			if( armed ) PORTD &= ~(1<<(5+current_alarm_index));
+			else PORTD |= (1<<(5+current_alarm_index));
+			return;
+		}
+		case 1:
+			alarm->time.hour += d;
+			break;
+		case 2:
+			alarm->time.min += d;
+			break;
+	}
+	alarm_screen[0] = alarm->time.hour/10;
+	alarm_screen[1] = alarm->time.hour%10;
+	alarm_screen[2] = alarm->time.min/10;
+	alarm_screen[3] = alarm->time.min%10;
+
 }
 
-void down_tick() {
+void up_handler() {
+	change(1);
+}
+
+void down_handler() {
+	change(-1);
 }
 
 int main() {
+
+	alarms[0].time.hour = 12;
+	alarms[0].time.min = 30;
+	alarms[1].time.hour = 7;
+	alarms[1].time.min = 15;
+	alarms[2].time.hour = 22;
+	alarms[2].time.min = 45;
 
 	//TIMER FOR MULTIPLEXING
 	TCCR0A |= (1<<WGM01);
@@ -82,9 +150,9 @@ int main() {
 
 	//BUTTONS
 	key_init();
-	button_t mode = { &BTN_MODE_PIN, BTN_MODE, 2, next_mode, next_mode };
-	button_t up = { &BTN_UP_PIN, BTN_UP, 2, up_tick, up_tick };
-	button_t down = { &BTN_DOWN_PIN, BTN_DOWN, 2, down_tick, down_tick };
+	button_t mode_btn = { &BTN_MODE_PIN, BTN_MODE, 2, next_mode, NULL };
+	button_t up_btn = { &BTN_UP_PIN, BTN_UP, 2, up_handler, NULL };
+	button_t down_btn = { &BTN_DOWN_PIN, BTN_DOWN, 2, down_handler, NULL };
 
 	BTN_MODE_IN;
 	BTN_MODE_HIGH;
@@ -111,16 +179,22 @@ int main() {
 
 	sei();
 	while( 1 ) {
-		key_press(&mode);
-		key_press(&up);
-		key_press(&down);
+		key_press(&mode_btn);
+		key_press(&up_btn);
+		key_press(&down_btn);
 
 		if( !Timer2 ) {
 			blink ^= 1;
 			Timer2 = 33;
 		}
+
+		if( !Timer3 ) {
+			display = main_screen;
+			blink_mask = 0b0000;
+			mode = 0;
+			Timer3 = 1000;
+		}
 	}
 
 	return 0;
 }
-
