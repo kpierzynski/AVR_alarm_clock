289047de27bcd947283140375edbcb09b1122c34
kpierzynski <kpierzynski@hotmail.com>
2022-10-12 19:21:38 +0200

    Separating clock.c from ic

--- clock.c
+++ clock.c
@@ -7,57 +7,9 @@ void register_clock_out_1hz( void (*callback)(void) ) {
 	clock_out_1hz_callback = callback;
 }
 
-static void clock_send( uint8_t addr, uint8_t data ) {
-	i2c_start();
-	i2c_write( DS1307_ADDR << 1 | 0 );
-	i2c_write( addr );
-	i2c_write( data );
-	i2c_stop();
-}
-
-static uint8_t clock_read( uint8_t addr ) {
-	register uint8_t data;
-	i2c_start();
-	i2c_write( DS1307_ADDR << 1 | 0 );
-	i2c_write( addr );
-	i2c_start();
-	i2c_write( DS1307_ADDR << 1 | 1 );
-	data = i2c_read( NACK );
-	i2c_stop();
-
-	return data;
-}
-
-void clock_init() {
-	i2c_init( 100 );
-
-	DDRD &= ~(1<<PD2);
-	PORTD |= (1<<PD2);
-
-	EICRA |= (1<<ISC01)|(1<<ISC00);		//RISING EDGE
-	EIMSK |= (1<<INT0);
-
-	clock_send( CONTROL_ADDR, 0b00010000 );					//SQUARE 1HZ WAVE (check that)
-	clock_send( CH_ADDR, clock_read(CH_ADDR) & 0b01111111 );		//START OSCILATOR
-	clock_send( HOURS_ADDR, clock_read(HOURS_ADDR) & 0b00111111 );		//SET 24HOUR MODE
-}
-
-static uint8_t bcd2dec( uint8_t bcd ) {
-	return ((((bcd) >> 4) & 0x0F) * 10 ) + ((bcd) & 0x0F);
-}
-
-static uint8_t dec2bcd( uint8_t dec ) {
-	return ( (dec/10) << 4 ) | ( dec % 10 );
-}
-
-void clock_time( time_t * time ) {
-	uint8_t hr = clock_read(HOURS_ADDR);
-	hr = bcd2dec( hr );
-	uint8_t min = clock_read(MINUTES_ADDR);
-	min = bcd2dec( min );
-
-	time->min = min;
-	time->hour = hr;
+void clock_update_time( time_t * time ) {
+	uint8_t ss;
+	ds1307_get_time( &time->hour, &time->min, &ss);
 }
 
 void clock_event() {
@@ -65,8 +17,4 @@ void clock_event() {
 		if( clock_out_1hz_callback ) clock_out_1hz_callback();
 		call_callback = 0;
 	}
-}
-
-ISR( INT0_vect ) {
-	call_callback = 1;
 }
--- clock.h
+++ clock.h
@@ -4,25 +4,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "i2c.h"
+#include "ds1307.h"
 #include "common.h"
 
-#define DS1307_ADDR	0x68
-
-#define CH_ADDR		0x00
-
-#define SECONDS_ADDR	0x00
-#define MINUTES_ADDR	0x01
-#define HOURS_ADDR	0x02
-
-#define DAY_ADDR	0x03
-#define DATE_ADDR	0x04
-#define MONTH_ADDR	0x05
-#define YEARS_ADDR	0x06
-
-#define CONTROL_ADDR	0x07
-
-#define RAM_ADDR	0x08
 
 void register_clock_out_1hz( void (*callback)(void) );
 void clock_init();
